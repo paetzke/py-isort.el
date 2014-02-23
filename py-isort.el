93a7309625f283acd83dd93fbf2a3dacd5c60efd
Syohei YOSHIDA <syohex@gmail.com>
2014-02-23 18:25:24 +0900

    Load cl-lib for `incf` and `decf`

--- py-isort.el
+++ py-isort.el
@@ -5,7 +5,9 @@
 ;; Author: Friedrich Paetzke <f.paetzke@gmail.com>
 ;; URL: https://github.com/paetzke/py-isort
 ;; Version: 0.1
+;; Package-Requires: ((cl-lib "0.4"))
 
+(require 'cl-lib)
 
 (defun py-isort-apply-rcs-patch (patch-buffer)
   "Apply an RCS-formatted diff from PATCH-BUFFER to the current buffer."
@@ -27,7 +29,7 @@
                 (forward-line len)
                 (let ((text (buffer-substring start (point))))
                   (with-current-buffer target-buffer
-                    (decf line-offset len)
+                    (cl-decf line-offset len)
                     (goto-char (point-min))
                     (forward-line (- from len line-offset))
                     (insert text)))))
@@ -35,7 +37,7 @@
               (with-current-buffer target-buffer
                 (goto-char (point-min))
                 (forward-line (- from line-offset 1))
-                (incf line-offset len)
+                (cl-incf line-offset len)
                 (kill-whole-line len)))
              (t
               (error "invalid rcs patch or internal error in py-isort-apply-rcs-patch")))))))))
